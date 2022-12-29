9bc388ae2f3c5e6092f1d90cf75c958e9b241a0b
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-12-29 17:06:18 +0700

    create node class

--- Act8/Program.cs
+++ Act8/Program.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace Act8
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+
+        public Node(string i, Node l, Node r)
         {
+            info = i;
+            leftchild = l;
+            rightchild = r;
         }
     }
 }
-
